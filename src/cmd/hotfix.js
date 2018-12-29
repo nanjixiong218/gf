f63139a4d8a604f985a316bfd9a6ca0c3ad2c09f
xiachi <xhy@asman.com.cn>
2018-12-29 10:29:08 +0800

    feature : hotfixed

--- src/cmd/hotfix.js
+++ src/cmd/hotfix.js
@@ -1,9 +1,15 @@
 const sh = require('shelljs')
 const child_process = require('child_process')
 const gitRev = require('git-rev-sync')
-
+const pkg = require('../../package.json')
+const fsp = require('fs-promise')
 module.exports = function(program, baseInfo) {
   // 这里是要打tag的，所有patch也需要有规则
   const timestamp = new Date().getTime()
-  sh.exec(`git flow hotfix start ${baseInfo.version}-patch-${timestamp}`)  
+  const patchVersion = `${baseInfo.version}-patch-${timestamp}`
+  sh.exec(`git flow hotfix start ${patchVersion}`)
+  // 修改 package.json 的 version: ${baseInfo.version}-patch-${timestamp} 
+  pkg.version = patchVersion
+  const pkgString = JSON.stringify(pkg)
+  fsp.writeFile('../../package.json', 'utf-8', pkgString) 
 }
