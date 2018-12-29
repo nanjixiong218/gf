7f788f2b10ec09b805d480eb55bb76c0af9c9433
xiachi <xhy@asman.com.cn>
2018-12-29 11:07:51 +0800

    fixed : haha

--- src/cmd/init.js
+++ src/cmd/init.js
@@ -5,6 +5,7 @@ module.exports = function(program) {
   // console.log(sh.exec('git flow init')) // TODO: bug for shelljs: https://github.com/shelljs/shelljs/issues/424
   try{
     child_process.execSync('git flow init', {stdio: 'inherit'})
+    // TODO: 执行 push -u 
   }catch(e) {
     console.error(e)
   }
