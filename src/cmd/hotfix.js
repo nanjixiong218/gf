26071441103a388420dd86bed050e6dec098721c
xiachi <xhy@asman.com.cn>
2018-12-29 11:10:32 +0800

    feature : haha

--- src/cmd/hotfix.js
+++ src/cmd/hotfix.js
@@ -4,11 +4,11 @@ const fsp = require('fs-promise')
 const path = require('path')
 
 module.exports = function(program, baseInfo) {
-  // 这里是要打tag的，所有patch也需要有规则
   const timestamp = new Date().getTime()
   const patchVersion = `${baseInfo.version}-patch-${timestamp}`
   sh.exec(`git flow hotfix start ${patchVersion}`)
-  // 修改 package.json 的 version: ${baseInfo.version}-patch-${timestamp} 
+  sh.exec(`git push -u origin hotfix/${patchVersion}`)
+  // 修改 package.json 的 version: patchVersion
   pkg.version = patchVersion
   const pkgString = JSON.stringify(pkg, null, 2)
   fsp.writeFile(path.join(baseInfo.cwd, 'package.json'), pkgString, 'utf-8') 
