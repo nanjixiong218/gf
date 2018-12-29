4ce8688a7539b48323a68503b0c133196f43d7b3
xiachi <xhy@asman.com.cn>
2018-12-29 09:53:34 +0800

    feature : name

--- src/cmd/feature.js
+++ src/cmd/feature.js
@@ -3,5 +3,6 @@ const child_process = require('child_process')
 const gitRev = require('git-rev-sync')
 
 module.exports = function(program) {
+  const name = program.feature
   sh.exec(`git flow feature start ${name}`)
 }
