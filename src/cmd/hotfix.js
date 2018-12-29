15443824fcb75405ac5ad0a73dd05d9c131bbe57
xiachi <xhy@asman.com.cn>
2018-12-29 10:14:39 +0800

    feature : baseInfo

--- src/cmd/hotfix.js
+++ src/cmd/hotfix.js
@@ -2,8 +2,8 @@ const sh = require('shelljs')
 const child_process = require('child_process')
 const gitRev = require('git-rev-sync')
 
-module.exports = function(program) {
+module.exports = function(program, baseInfo) {
   // 这里是要打tag的，所有patch也需要有规则
   const timestamp = new Date().getTime()
-  sh.exec(`git flow hotfix start ${this.version}-patch-${timestamp}`)  
+  sh.exec(`git flow hotfix start ${baseInfo.version}-patch-${timestamp}`)  
 }
